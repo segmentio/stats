242aeee04744ddc4d6e31ac35446c50bfac713ac
Achille Roussel <achille@segment.com>
2016-08-24 03:17:19 -0700

    working on /proc/<pid>/stat

--- procstats/proc_linux.go
+++ procstats/proc_linux.go
@@ -5,6 +5,7 @@ import "github.com/segmentio/stats/procstats/linux"
 func collectProcMetrics(pid int) (m proc, err error) {
 	code := [...](func(int, *proc) error){
 		collectProcLimits,
+		collectProcStat,
 		collectOpenFileCount,
 	}
 
@@ -20,6 +21,21 @@ func collectProcLimits(pid int, m *proc) (err error) {
 
 	if limits, err = linux.GetProcLimits(pid); err == nil {
 		m.files.max = limits.OpenFiles.Soft
+	}
+
+	return
+}
+
+func collectProcStat(pid int, m *proc) (err error) {
+	var stat linux.ProcStat
+
+	if stat, err = linux.GetProcStat(pid); err == nil {
+		// TODO: extract CPU, (see sysconf)
+
+		m.memory.majorPageFaults = uint64(stat.Majflt)
+		m.memory.minorPageFaults = uint64(stat.Minflt)
+
+		m.threads.num = uint64(stat.NumThreads)
 	}
 
 	return
