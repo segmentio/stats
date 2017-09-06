e8333ded0f82a871779960cd3ec71bb2e83e29d3
Achille Roussel <achille@segment.com>
2017-09-06 11:33:53 -0700

    more accurate GC stats

--- procstats/go_test.go
+++ procstats/go_test.go
@@ -1,9 +1,7 @@
 package procstats
 
 import (
-	"reflect"
 	"runtime"
-	"runtime/debug"
 	"testing"
 	"time"
 
@@ -30,60 +28,11 @@ func TestGoMetrics(t *testing.T) {
 		}
 
 		h.Clear()
-		runtime.GC()
+
+		for j := 0; j <= i; j++ {
+			runtime.GC() // to get non-zero GC stats
+		}
+
 		time.Sleep(10 * time.Millisecond)
-	}
-}
-
-func TestStripOutdatedGCPauses(t *testing.T) {
-	now := time.Now()
-
-	gc := &debug.GCStats{}
-	gc.LastGC = now.Add(-time.Second) // 1s ago
-	gc.NumGC = 10                     // 10th GC pass
-	gc.PauseTotal = time.Millisecond  // 1ms pauses total
-	gc.Pause = []time.Duration{
-		100 * time.Microsecond,
-		100 * time.Microsecond,
-		100 * time.Microsecond,
-		100 * time.Microsecond,
-		100 * time.Microsecond,
-		100 * time.Microsecond,
-		100 * time.Microsecond,
-		100 * time.Microsecond,
-		100 * time.Microsecond,
-		100 * time.Microsecond,
-	}
-	gc.PauseEnd = []time.Time{
-		now.Add(-time.Second).Add(-100 * time.Microsecond),
-		now.Add(-time.Second).Add(-200 * time.Microsecond),
-		now.Add(-time.Second).Add(-300 * time.Microsecond),
-		now.Add(-time.Second).Add(-400 * time.Microsecond),
-		now.Add(-time.Second).Add(-500 * time.Microsecond),
-		now.Add(-time.Second).Add(-600 * time.Microsecond),
-		now.Add(-time.Second).Add(-700 * time.Microsecond),
-		now.Add(-time.Second).Add(-800 * time.Microsecond),
-		now.Add(-time.Second).Add(-900 * time.Microsecond),
-		now.Add(-time.Second).Add(-1 * time.Millisecond),
-	}
-
-	gc.PauseQuantiles = nil // ???
-
-	stripOutdatedGCPauses(gc, 8) // last observed the 8th pass
-
-	if !reflect.DeepEqual(gc, &debug.GCStats{
-		LastGC:     now.Add(-time.Second),
-		NumGC:      10,
-		PauseTotal: time.Millisecond,
-		Pause: []time.Duration{
-			100 * time.Microsecond,
-			100 * time.Microsecond,
-		},
-		PauseEnd: []time.Time{
-			now.Add(-time.Second).Add(-100 * time.Microsecond),
-			now.Add(-time.Second).Add(-200 * time.Microsecond),
-		},
-	}) {
-		t.Errorf("invalid gc stats after stripping outdated pauses:\n%#v", *gc)
 	}
 }
