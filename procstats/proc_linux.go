56e96ec22fc2945af89aae63f93a7f72acf120ec
Achille Roussel <achille@segment.com>
2018-07-28 21:12:51 -0700

    fix reading CPU stats of other processes

--- procstats/proc_linux.go
+++ procstats/proc_linux.go
@@ -1,24 +1,61 @@
 package procstats
 
 import (
+	"io/ioutil"
 	"os"
+	"strconv"
+	"strings"
+	"sync"
 	"syscall"
 	"time"
 
 	"github.com/segmentio/stats/procstats/linux"
 )
 
+var (
+	clockTickOnce  sync.Once
+	clockTickValue uint64
+	clockTickError error
+)
+
+func clockTick() (uint64, error) {
+	s, err := getconf("CLK_TCK")
+	if err != nil {
+		return 0, err
+	}
+	return strconv.ParseUint(strings.TrimSpace(s), 10)
+}
+
+func getconf(name string) (string, error) {
+	file, err := exec.LookupPath("getconf")
+	if err != nil {
+		return 0, err
+	}
+
+	r, w, err := os.Pipe()
+	if err != nil {
+		return 0, nil
+	}
+	defer r.Close()
+	defer w.Close()
+
+	p, err := os.StartProcess(file, []string{"getconf", name}, &os.ProcAttr{
+		Files: []*os.File{os.Stdin, stdout, os.Stderr},
+	})
+	if err != nil {
+		return 0, err
+	}
+
+	b, err := ioutil.ReadAll(r)
+	p.Wait()
+	return string(b), err
+}
+
 func collectProcInfo(pid int) (info ProcInfo, err error) {
 	defer func() { err = convertPanicToError(recover()) }()
 
-	pagesize := uint64(syscall.Getpagesize())
-	rusage := syscall.Rusage{}
-
-	if pid == os.Getpid() {
-		check(syscall.Getrusage(syscall.RUSAGE_SELF, &rusage))
-	} else {
-		// TODO: figure out how to get accurate stats for other processes
-	}
+	var pagesize = uint64(syscall.Getpagesize())
+	var cpu CPUInfo
 
 	memoryLimit, err := linux.GetMemoryLimit(pid)
 	check(err)
@@ -38,11 +75,24 @@ func collectProcInfo(pid int) (info ProcInfo, err error) {
 	fds, err := linux.GetOpenFileCount(pid)
 	check(err)
 
-	info = ProcInfo{
-		CPU: CPUInfo{
+	if pid == os.Getpid() {
+		rusage := syscall.Rusage{}
+		check(syscall.Getrusage(syscall.RUSAGE_SELF, &rusage))
+		cpu = CPUInfo{
 			User: time.Duration(rusage.Utime.Nano()),
 			Sys:  time.Duration(rusage.Stime.Nano()),
-		},
+		}
+	} else {
+		clockTickOnce.Do(func() { clockTick, clockTickError = clockTick() })
+		check(clockTickError)
+		cpu = CPUInfo{
+			User: time.Duration(stat.Utime) * (time.Nanosecond / time.Duration(clockTick)),
+			User: time.Duration(stat.Stime) * (time.Nanosecond / time.Duration(clockTick)),
+		}
+	}
+
+	info = ProcInfo{
+		CPU: cpu,
 
 		Memory: MemoryInfo{
 			Available:       memoryLimit,
@@ -51,8 +101,8 @@ func collectProcInfo(pid int) (info ProcInfo, err error) {
 			Shared:          pagesize * statm.Share,
 			Text:            pagesize * statm.Text,
 			Data:            pagesize * statm.Data,
-			MajorPageFaults: uint64(rusage.Majflt),
-			MinorPageFaults: uint64(rusage.Minflt),
+			MajorPageFaults: stat.Majflt,
+			MinorPageFaults: stat.Minflt,
 		},
 
 		Files: FileInfo{
