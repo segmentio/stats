c2960098a89fc6257d2a0672f0a8e3c7fbc1133b
Michael S. Fischer <michael.fischer@segment.com>
2018-08-10 12:11:05 -0700

    delaystats: add helpful error message

--- procstats/delaystats_linux.go
+++ procstats/delaystats_linux.go
@@ -1,6 +1,8 @@
 package procstats
 
 import (
+	"syscall"
+
 	"github.com/mdlayher/taskstats"
 )
 
@@ -9,7 +11,12 @@ func collectDelayInfo(pid int) (info DelayInfo, err error) {
 	check(err)
 
 	stats, err := client.PID(pid)
-	check(err)
+	if err != nil {
+		if err1, ok := err.(syscall.Errno); ok && err1 == syscall.EPERM {
+			panic("Failed to open Netlink socket: permission denied.  Ensure CAP_NET_RAW is enabled for this process, or run as root.")
+		}
+		check(err)
+	}
 
 	info.BlockIODelay = stats.BlockIODelay
 	info.CPUDelay = stats.CPUDelay
