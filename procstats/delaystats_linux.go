cb72c3dcf1ba958715bcd416453e6a4fd50202b4
Michael S. Fischer <michael.fischer@segment.com>
2018-08-13 11:26:57 -0700

    Fix bug in error assignment

--- procstats/delaystats_linux.go
+++ procstats/delaystats_linux.go
@@ -10,13 +10,13 @@ import (
 func collectDelayInfo(pid int) (info DelayInfo, err error) {
 	client, err := taskstats.New()
 	if err == syscall.ENOENT {
-		err := errors.New("Failed to communicate with taskstats Netlink family.  Ensure this program is not running in a network namespace.")
+		err = errors.New("Failed to communicate with taskstats Netlink family.  Ensure this program is not running in a network namespace.")
 	}
 	check(err)
 
 	stats, err := client.PID(pid)
 	if err == syscall.EPERM {
-		err := errors.New("Failed to open Netlink socket: permission denied.  Ensure CAP_NET_RAW is enabled for this process, or run it with root privileges.")
+		err = errors.New("Failed to open Netlink socket: permission denied.  Ensure CAP_NET_RAW is enabled for this process, or run it with root privileges.")
 	}
 	check(err)
 
