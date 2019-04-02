17e5e763373e9d6a80804ff4dab85a842ad03a09
Mike Nichols <nichols.mike.s@gmail.com>
2019-04-02 09:56:53 -0600

    Add support for tags per request: InVisionApp fork (#104)
    * Add support for per request tags
    
    * support later ver of golang context api, apply current golang rules for context keys, and make name change requested in PR.

--- httpstats/transport.go
+++ httpstats/transport.go
@@ -4,7 +4,23 @@ import (
 	"net/http"
 	"time"
 
+	"context"
 	"github.com/segmentio/stats"
+)
+
+// tagsKey is a value for use with context.WithValue. It's used as
+// a pointer so it fits in an interface{} without allocation. This technique
+// for defining context keys was copied from Go 1.7's new use of context in net/http.
+type tagsKey struct{}
+
+// String is Stringer implementation
+func (k *tagsKey) String() string {
+	return "stats_tags_context_key"
+}
+
+// contextKeyReqTags is contextKey for tags
+var (
+	contextKeyReqTags = &tagsKey{}
 )
 
 // NewTransport wraps t to produce metrics on the default engine for every request
@@ -27,12 +43,27 @@ type transport struct {
 	eng       *stats.Engine
 }
 
+// RequestWithTags returns a shallow copy of req with its context changed with this provided tags
+// so the they can be used later during the RoundTrip in the metrics recording.
+// The provided ctx must be non-nil.
+func RequestWithTags(req *http.Request, tags ...stats.Tag) *http.Request {
+	ctx := req.Context()
+	ctx = context.WithValue(ctx, contextKeyReqTags, tags)
+	return req.WithContext(ctx)
+}
+
+// RoundTrip implements http.RoundTripper
 func (t *transport) RoundTrip(req *http.Request) (res *http.Response, err error) {
 	start := time.Now()
 	rtrip := t.transport
+	eng := t.eng
 
 	if rtrip == nil {
 		rtrip = http.DefaultTransport
+	}
+
+	if tags, ok := req.Context().Value(contextKeyReqTags).([]stats.Tag); ok {
+		eng = eng.WithTags(tags...)
 	}
 
 	if req.Body == nil {
@@ -42,7 +73,7 @@ func (t *transport) RoundTrip(req *http.Request) (res *http.Response, err error)
 	m := &metrics{}
 
 	req.Body = &requestBody{
-		eng:     t.eng,
+		eng:     eng,
 		req:     req,
 		metrics: m,
 		body:    req.Body,
@@ -50,14 +81,15 @@ func (t *transport) RoundTrip(req *http.Request) (res *http.Response, err error)
 	}
 
 	res, err = rtrip.RoundTrip(req)
-	req.Body.Close() // safe guard, the transport should have done it already
+	// safe guard, the transport should have done it already
+	req.Body.Close() // nolint
 
 	if err != nil {
 		m.observeError(time.Now().Sub(start))
-		t.eng.ReportAt(start, m)
+		eng.ReportAt(start, m)
 	} else {
 		res.Body = &responseBody{
-			eng:     t.eng,
+			eng:     eng,
 			res:     res,
 			metrics: m,
 			body:    res.Body,
