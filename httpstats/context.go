2e6135bc21924fd308356a65da82586bdf7a7b8c
Alan Braithwaite <alan@segment.com>
2019-09-19 16:38:16 -0700

    httpstats: support adding arbitrary tags to original request metrics

---
+++ httpstats/context.go
@@ -0,0 +1,62 @@
+package httpstats
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/segmentio/stats"
+)
+
+// RequestWithTags returns a shallow copy of req with its context changed with this provided tags
+// so the they can be used later during the RoundTrip in the metrics recording.
+// The provided ctx must be non-nil.
+func RequestWithTags(req *http.Request, tags ...stats.Tag) *http.Request {
+	ctx := req.Context()
+	ctx = context.WithValue(ctx, contextKeyReqTags, &tags)
+	return req.WithContext(ctx)
+}
+
+// AddTagsToRequest adds the given tags to the request if the reqeuest context
+// has already been initialized with tags. It returns a bool which is true if
+// the context value exists and they were added, false otherwise.
+func AddTagsToRequest(req *http.Request, tags ...stats.Tag) bool {
+	cur := getReqTagsSlice(req)
+	if cur == nil {
+		return false
+	}
+	*cur = append(*cur, tags...)
+	return true
+}
+
+// Tags returns a copy of the tags on the request, if they exist.  Otherwise it
+// returns nil.
+func Tags(req *http.Request) []stats.Tag {
+	if x := getReqTagsSlice(req); x != nil {
+		ret := make([]stats.Tag, len(*x))
+		copy(ret, *x)
+		return ret
+	}
+	return nil
+}
+
+func getReqTagsSlice(req *http.Request) *[]stats.Tag {
+	if tags, ok := req.Context().Value(contextKeyReqTags).(*[]stats.Tag); ok {
+		return tags
+	}
+	return nil
+}
+
+// tagsKey is a value for use with context.WithValue. It's used as
+// a pointer so it fits in an interface{} without allocation. This technique
+// for defining context keys was copied from Go 1.7's new use of context in net/http.
+type tagsKey struct{}
+
+// String is Stringer implementation
+func (k *tagsKey) String() string {
+	return "stats_tags_context_key"
+}
+
+// contextKeyReqTags is contextKey for tags
+var (
+	contextKeyReqTags = &tagsKey{}
+)
---
+++ httpstats/context_test.go
@@ -0,0 +1,37 @@
+package httpstats
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/segmentio/stats"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestContextTags(t *testing.T) {
+	// dummy request
+	x := httptest.NewRequest(http.MethodGet, "http://example.com/blah", nil)
+
+	y := RequestWithTags(x)
+	assert.Equal(t, 0, len(Tags(y)), "Initialize request tags context value")
+	AddTagsToRequest(y, stats.T("asdf", "qwer"))
+	assert.Equal(t, 1, len(Tags(y)), "Adding tags should result new tags")
+	assert.Equal(t, 0, len(Tags(x)), "Original request should have no tags (because no context with key)")
+
+	// create a child request which creates a child context
+	z := y.WithContext(context.WithValue(y.Context(), "not", "important"))
+	assert.Equal(t, 1, len(Tags(z)), "We should still be able to see original tags")
+
+	// Add tags to the child context's reference to the original tag slice
+	AddTagsToRequest(z, stats.T("zxcv", "uiop"))
+	assert.Equal(t, 2, len(Tags(z)), "Updating tags should update local reference")
+	assert.Equal(t, 2, len(Tags(y)), "Updating tags should update parent reference")
+	assert.Equal(t, 0, len(Tags(x)), "Updating tags should not appear on original request")
+
+	AddTagsToRequest(z, stats.T("a", "k"), stats.T("b", "k"), stats.T("c", "k"), stats.T("d", "k"), stats.T("e", "k"), stats.T("f", "k"))
+	assert.Equal(t, 8, len(Tags(z)), "Updating tags should update local reference")
+	assert.Equal(t, 8, len(Tags(y)), "Updating tags should update parent reference")
+	assert.Equal(t, 0, len(Tags(x)), "Updating tags should not appear on original request")
+}
