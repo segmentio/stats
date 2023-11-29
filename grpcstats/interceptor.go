da6fcd070665142442ec77ca0b80493fea476728
msaunders-twilio <msaunders.twilio@gmail.com>
2023-11-29 10:48:57 -0500

    Add metadata bit

--- grpcstats/interceptor.go
+++ grpcstats/interceptor.go
@@ -2,25 +2,30 @@ package grpcstats
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 )
 
 func MetricsServerInterceptor() grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
-		// // Get the metadata from the incoming context
-		// md, ok := metadata.FromIncomingContext(ctx)
-		// if !ok {
-		// 	return nil, fmt.Errorf("couldn't parse incoming context metadata")
-		// }
-
-		// m := &metrics{}
 
 		log.Printf("-->> metrics interceptor hit")
 		log.Print(ctx)
 		log.Print(req)
 		log.Print(info)
+
+		// Get the metadata from the incoming context
+		md, ok := metadata.FromIncomingContext(ctx)
+		if !ok {
+			return nil, fmt.Errorf("couldn't parse incoming context metadata")
+		}
+
+		fmt.Printf("md: %v\n", md)
+
+		// m := &metrics{}
 
 		h, err := handler(ctx, req)
 
