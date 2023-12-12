9276b698449817823ffd27f88b99cbbc27e3b9b7
msaunders-twilio <msaunders.twilio@gmail.com>
2023-12-12 14:14:42 -0500

    Comment out transport for now

--- grpcstats/transport.go
+++ grpcstats/transport.go
@@ -1,28 +1,28 @@
-package grpcstats
+// package grpcstats
 
-import (
-	"net/http"
-	"time"
+// import (
+// 	"net/http"
+// 	"time"
 
-	"github.com/segmentio/stats/v4"
-)
+// 	"github.com/segmentio/stats/v4"
+// )
 
-type transport {
-	transport http.RoundTripper
-	eng *stats.Engine
-}
+// type transport {
+// 	transport http.RoundTripper
+// 	eng *stats.Engine
+// }
 
-// NewTransport wraps t to produce metrics on the default engine for every request
-// sent and every response received.
-func NewTransport(t http.RoundTripper) http.RoundTripper {
-	return NewTransportWith(stats.DefaultEngine, t)
-}
+// // NewTransport wraps t to produce metrics on the default engine for every request
+// // sent and every response received.
+// func NewTransport(t http.RoundTripper) http.RoundTripper {
+// 	return NewTransportWith(stats.DefaultEngine, t)
+// }
 
-// NewTransportWith wraps t to produce metrics on eng for every request sent and
-// every response received.
-func NewTransportWith(eng *stats.Engine, t http.RoundTripper) http.RoundTripper {
-	return &transport{
-		transport: t,
-		eng:       eng,
-	}
-}
+// // NewTransportWith wraps t to produce metrics on eng for every request sent and
+// // every response received.
+// func NewTransportWith(eng *stats.Engine, t http.RoundTripper) http.RoundTripper {
+// 	return &transport{
+// 		transport: t,
+// 		eng:       eng,
+// 	}
+// }
